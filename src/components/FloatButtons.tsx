6af6308bef29fc09683f70ad33fa26e3402ca263
edusebass <eduardoalmachi123@hotmail.com>
2025-06-17 13:15:24 -0500

    ?

--- src/components/FloatButtons.tsx
+++ src/components/FloatButtons.tsx
@@ -66,7 +66,7 @@ const FloatButtons = () => {
 
       {/* Google Maps y Teléfono a la derecha */}
       <div
-        className="fixed bottom-2/3 right-2 flex flex-col gap-1 z-50 transition-transform duration-300"
+        className="fixed bottom-1/4 right-40 flex flex-col gap-1 z-50 transition-transform duration-300"
         style={{
           transform: `translateY(${offset}px)`,
         }}
