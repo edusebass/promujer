75c25a5e59671a3e3bc5363bcbfc33a72c27d4aa
edusebass <eduardoalmachi123@hotmail.com>
2025-06-17 12:53:29 -0500

    ?

--- src/components/FloatButtons.tsx
+++ src/components/FloatButtons.tsx
@@ -44,7 +44,7 @@ const FloatButtons = () => {
 
   return (
     <div
-      className="fixed bottom-12 left-4 flex flex-col gap-1 z-50 transition-transform duration-300"
+      className="fixed bottom-2 left-2 flex flex-col gap-1 z-50 transition-transform duration-300"
       style={{
         transform: `translateY(${offset}px)`,
       }}
